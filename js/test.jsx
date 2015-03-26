980c9d7a0fb48bed5222430eb7a4b7531ec1701f
Ryan Brooks <ryanbrooksis@gmail.com>
2015-03-26 17:50:17 +0000

    :memo: Sample React code

---
+++ js/test.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+module.exports = React.createClass({
+    render: function() {
+      return (
+        <div className="commentBox">
+          Hello, world! I am a CommentBox.
+        </div>
+      );
+    }
+  });
