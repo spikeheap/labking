63d2892714dd2d20255b987f01dada2a8848cfff
Ryan Brooks <ryanbrooksis@gmail.com>
2015-04-08 19:23:04 +0100

    Adds form inputs for #9.

--- js/components/RecordDataSet.jsx
+++ js/components/RecordDataSet.jsx
@@ -2,6 +2,7 @@
   "use strict";
 
   var React = require("react");
+  var forms = require("newforms");
 
   var RecordDataSet = React.createClass({
 
@@ -11,20 +12,61 @@
       participantDataSet: React.PropTypes.object
     },
 
+    getDefaultProps: function() {
+      return {
+        participantDataSet: {}
+      };
+    },
+
+    handleSubmit: function(e) {
+      e.preventDefault();
+      console.log("save!");
+
+      var form = this.refs.updateRecordDataSetForm.getForm();
+      var isValid = form.validate();
+      if (isValid) {
+        console.log("VALID!");
+        console.log(form.cleanedData);
+      }else{
+        console.log(form.errors());
+      }
+    },
+
+    handleReset: function(e) {
+      e.preventDefault();
+      console.log("reset!");
+
+      var form = this.refs.updateRecordDataSetForm.getForm();
+      form.reset();
+    },
+
     render: function() {
 
-      var fields = this.props.dataSetMetaData.columns.map( (column) => {
-        var dataSet = (this.props.participantDataSet !== undefined) ? this.props.participantDataSet : {};
-        return (<div className="pure-control-group">
-                  <label htmlFor="{column.Label}">{column.Label}</label>
-                  <input id="{column.Label}" type="text" value={dataSet[column.Name]} />
-              </div>);
+      var fieldSet = {};
+      this.props.dataSetMetaData.columns.forEach( (column) => {
+        console.log(column.RangeURI);
+        if(column.RangeURI === "http://www.w3.org/2001/XMLSchema#dateTime"){
+          console.log(column.Name, "date");
+          fieldSet[column.Name] = forms.DateField({initial: this.props.participantDataSet[column.Name] || '', required: false, label: column.Label})
+        }else{
+          console.log(column.Name, "text");
+          fieldSet[column.Name] = forms.CharField({initial: this.props.participantDataSet[column.Name] || '', required: false, label: column.Label})
+        }
+        
+        // TODO date picker {widget: forms.DatePicker}
+        // TODO multi-select
       });
 
+      var RecordForm = forms.Form.extend(fieldSet);
+
       return (
-        <form className="pure-form pure-form-aligned">
+        <form onSubmit={this.handleSubmit}>
           <fieldset>
-            {fields}
+            <forms.RenderForm form={RecordForm} ref="updateRecordDataSetForm"/>
+          </fieldset>
+          <fieldset>
+            <input type="submit" value="Save changes" className="pure-button btn-success"/>
+            <a className="pure-button btn-error pull-right" onClick={this.handleReset}>Reset form</a>
           </fieldset>
         </form>
       );
