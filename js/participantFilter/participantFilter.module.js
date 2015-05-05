1799c651c34af42027b654e765a05dce8ffb6071
Ryan Brooks <ryanbrooksis@gmail.com>
2015-05-05 12:39:08 +0100

    Add Boolean and TextArea (multi-line) support. #36

--- js/participantFilter/participantFilter.module.js
+++ js/participantFilter/participantFilter.module.js
@@ -7,7 +7,7 @@ require('angular-utils-pagination');
 require('ui.utils');
 
 require('angular')
-  .module('labking.participantFilter', ['blocks.logger', 'labking.core', 'angularUtils.directives.dirPagination'])
+  .module('labking.participantFilter', ['blocks.logger', 'labking.core', 'angularUtils.directives.dirPagination', 'ui.indeterminate'])
   .directive('participantFilter', require('./participantFilter.directive'))
   .directive('participantRecord', require('./participantRecord.directive'))
   .directive('datasetView', require('./datasetView.directive'))
