a8abf13dbc755c1e3db1d73ceed95b9abe0cae60
Ryan Brooks <ryanbrooksis@gmail.com>
2015-06-01 10:37:56 +0100

    misc changes...

--- js/labking.module.js
+++ js/labking.module.js
@@ -2,11 +2,13 @@
 
 var angular = require('angular');
 
-require('./core/core.module'),
-require('./layout/layout.module')
+require('./core/core.module');
+require('./layout/layout.module');
+require('../bower_components/angular-animate/index.js');
 
 angular
   .module('labking', [
     'labking.core',
-    'labking.layout'
+    'labking.layout',
+    'ngAnimate'
   ]);
