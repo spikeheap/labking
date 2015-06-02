dfde1cdbdfa09890718d0cc5adbec75e0a20acb5
Ryan Brooks <ryanbrooksis@gmail.com>
2015-06-02 20:33:46 +0100

    Add re-filter on cohort load to prevent empty participant list when the cohort list loaded second.

--- js/participantFilter/participantFilter.directive.js
+++ js/participantFilter/participantFilter.directive.js
@@ -13,28 +13,52 @@ function ParticipantFilter($q, CohortService, ParticipantService) {
       bindToController: true,
       controller: function () {
         var self = this;
-        self.allParticipants = [];
-        $q.all([
-          ParticipantService.getParticipantList(),
-          ParticipantService.getParticipantKeyInfo()
-        ]).then(function(responses) {
-          var [participants, participantsKeyInfo] = responses;
-          self.allParticipants = participants.map(function(participant) {
-            participant.keyInfo = _.find(participantsKeyInfo, 'ParticipantId', participant.ParticipantId);
-            return participant;
-          });
-          filterParticipants();
-        });
 
+        self.selectedCohorts = {};
+        self.participantCount = participantCount;
+        self.isCohortSelected = isCohortSelected;
+        self.toggleCohort = toggleCohort;
+
+        self.participantSearchText = '';
+        self.participantSearchFilter = participantSearchFilter;
+
+        self.allParticipants = [];
         self.selectedParticipant = {};
-        self.selectParticipant = function(participant) {
+        self.selectParticipant = selectParticipant;
+        self.isParticipantSelected = isParticipantSelected;
+
+        activate();
+
+        function activate(){
+          $q.all([
+            ParticipantService.getParticipantList(),
+            ParticipantService.getParticipantKeyInfo()
+          ]).then(function(responses) {
+            var [participants, participantsKeyInfo] = responses;
+            self.allParticipants = participants.map(function(participant) {
+              participant.keyInfo = _.find(participantsKeyInfo, 'ParticipantId', participant.ParticipantId);
+              return participant;
+            });
+            filterParticipants();
+          });
+
+          CohortService.getCohorts().then(function(cohorts) {
+            self.cohorts = cohorts;
+            self.cohorts.forEach(function(cohort) {
+              self.selectedCohorts[cohort.rowid] = true;
+            });
+            filterParticipants();
+          });
+        }
+
+        function selectParticipant(participant) {
           self.selectedParticipant = participant;
           self.onParticipantSelect({participantId: participant.ParticipantId});
-        };
+        }
 
-        self.isParticipantSelected = function(participant){
+        function isParticipantSelected(participant){
           return participant && participant.ParticipantId && self.selectedParticipant.ParticipantId === participant.ParticipantId;
-        };
+        }
 
         function filterParticipants(){
           self.filteredParticipants = self.allParticipants.filter(function(candidateParticipant) {
@@ -42,8 +66,7 @@ function ParticipantFilter($q, CohortService, ParticipantService) {
           });
         }
 
-        self.participantSearchText = '';
-        self.participantSearchFilter = function(participant) {
+        function participantSearchFilter(participant) {
           return (
             fieldMatches(participant.ParticipantId, self.participantSearchText) ||
             fieldMatches(participant.keyInfo.NHSNumber, self.participantSearchText) ||
@@ -51,7 +74,7 @@ function ParticipantFilter($q, CohortService, ParticipantService) {
             fieldMatches(participant.keyInfo.FirstName, self.participantSearchText) ||
             fieldMatches(participant.keyInfo.LastName, self.participantSearchText)
           );
-        };
+        }
 
         function fieldMatches(field, term){
           return field && field.toUpperCase().indexOf(term.toUpperCase()) > -1;
@@ -63,24 +86,15 @@ function ParticipantFilter($q, CohortService, ParticipantService) {
          If a cohort is IN the filter this will remove the e
         **/
 
-        self.selectedCohorts = {};
-
-        self.toggleCohort = function(cohort) {
+        function toggleCohort(cohort) {
           self.selectedCohorts[cohort.rowid] = !self.selectedCohorts[cohort.rowid];
           filterParticipants();
-        };
-        self.isCohortSelected = function(cohort) {
+        }
+
+        function isCohortSelected(cohort) {
           return self.selectedCohorts[cohort.rowid];
-        };
+        }
 
-        CohortService.getCohorts().then(function(cohorts) {
-          self.cohorts = cohorts;
-          self.cohorts.forEach(function(cohort) {
-            self.selectedCohorts[cohort.rowid] = true;
-          });
-        });
-
-        self.participantCount = participantCount;
         function participantCount(cohort){
           return self.allParticipants.filter(function(participant) {
             return participant.Cohort === cohort.rowid;
