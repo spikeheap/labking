1113d8d922fca15167d897decb5383569dff5c73
Ryan Brooks <ryanbrooksis@gmail.com>
2015-06-16 11:46:17 +0100

    Fix labels using lookups (introduced in 0.0.4)

--- js/participantFilter/datasetView.directive.js
+++ js/participantFilter/datasetView.directive.js
@@ -15,18 +15,35 @@ function DatasetView() {
     controllerAs: 'vm',
     bindToController: true,
     /** @ngInject **/
-    controller: function($filter) {
+    controller: function($filter, $scope, DatasetMetadataService) {
       var self = this;
+      var _ = require('lodash');
 
+      self.lookups = {};
       self.getValue = getValue;
 
-      function getValue(row, column){
-        // if (column.LookupQuery && self.lookups[column.LookupQuery]) {
-        //   var val = _.find(self.lookups[column.LookupQuery].rows, 'Key', row[column.Name]);
-        //   return val === undefined ? '' : val.Label;
-        // }else
+      $scope.$watch(function () {
+        return self.dataset;
+      }, function (newVal) {
+        if(newVal && self.dataset.columns){
+          console.log("grabbing lookups");
+          self.dataset.columns
+            .filter(function(column) {
+              return !!column.LookupQuery;
+            })
+            .forEach(function (column) {
+              var lookupQuery = column.LookupQuery;
+              DatasetMetadataService.getLookups(lookupQuery)
+                .then((lookup) => self.lookups[lookupQuery] = lookup);
+            });
+        }
+      });
 
-        if (column.RangeURI === 'http://www.w3.org/2001/XMLSchema#dateTime'){
+      function getValue(row, column){
+        if (column.LookupQuery && self.lookups[column.LookupQuery]) {
+          var val = _.find(self.lookups[column.LookupQuery].rows, 'Key', row[column.Name]);
+          return val === undefined ? '' : val.Label;
+        }else if (column.RangeURI === 'http://www.w3.org/2001/XMLSchema#dateTime'){
           return $filter('date')(row[column.Name], 'shortDate');
         }else{
           return row[column.Name];
