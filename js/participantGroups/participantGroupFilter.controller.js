d035c0fe86982bfe446e03ea582e92b14151e9bc
Ryan Brooks <ryanbrooksis@gmail.com>
2015-09-15 18:11:52 +0100

    Toggle ignored categories

--- js/participantGroups/participantGroupFilter.controller.js
+++ js/participantGroups/participantGroupFilter.controller.js
@@ -9,6 +9,7 @@ function ParticipantGroupFilterController(ParticipantService, ParticipantGroupsS
   let self = this;
 
   let selectedGroups = {};
+  let ignoredCategories = {};
 
   self.toggleNotInCategory = toggleNotInCategory;
   self.isNotInCategorySelected = isNotInCategorySelected;
@@ -19,7 +20,8 @@ function ParticipantGroupFilterController(ParticipantService, ParticipantGroupsS
 
   self.selectAllGroups = selectAllGroups;
   self.excludeAllGroups = excludeAllGroups;
-  self.ignoreCategory = ignoreCategory;
+  self.toggleIgnoreCategory = toggleIgnoreCategory;
+  self.isCategoryIgnored = isCategoryIgnored;
 
   activate();
 
@@ -83,9 +85,13 @@ function ParticipantGroupFilterController(ParticipantService, ParticipantGroupsS
     emitChangeNotification();
   }
 
-  function ignoreCategory (categoryLabel) {
-    filterByCategory(categoryLabel, false);
-    selectedGroups[NOT_IN_PREFIX + categoryLabel] = true;
+  function toggleIgnoreCategory (categoryLabel) {
+    ignoredCategories[categoryLabel] = !ignoredCategories[categoryLabel];
+    emitChangeNotification();
+  }
+
+  function isCategoryIgnored (categoryLabel) {
+    return ignoredCategories[categoryLabel];
   }
 
 
@@ -107,19 +113,23 @@ function ParticipantGroupFilterController(ParticipantService, ParticipantGroupsS
 
   function getSelectedParticipantIDs () {
 
+    var categoriesToFilter = _.filter(self.categories, function (groups, categoryLabel) {
+      return !ignoredCategories[categoryLabel];
+    });
+
     // If we have no groups the filter can't be applied, so return everything
-    if(!self.categories || _.keys(self.categories).length === 0){
+    if(_.keys(categoriesToFilter).length === 0){
       return self.allParticipants;
     }
 
-    var participantsInSelectedGroups = _.map(self.categories, function (groups) {
+    var participantsInSelectedGroups = _.map(categoriesToFilter, function (groups) {
       return _.filter(groups, self.isGroupSelected).map(function (group) {
         return group.participantList;
       });
     });
 
     var categoriesSelected = _.filter(self.categories, function (groups, categoryLabel) {
-      return isNotInCategorySelected(categoryLabel);
+      return !ignoredCategories[categoryLabel] && isNotInCategorySelected(categoryLabel);
     });
 
     var nonCategoryParticipants = _.uniq(_.flattenDeep(_.map(categoriesSelected, function (groups) {
